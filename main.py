3f73c14db222156435355ab12fe5e6edb91b6a4a
MrNavaStar <Mr.NavaStar@gmail.com>
2023-05-14 20:29:24 -0600

    fixes?

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import sys
 import uvicorn
 from fastapi import FastAPI
 from fastapi_amis_admin.admin import AdminSite, Settings
@@ -32,40 +31,33 @@ async def websocket(ws: WebSocket):
 
 @scheduler.scheduled_job('interval', seconds=5)
 def getTemps():
-    print("doing temps")
     sensors = DS18B20.get_all_sensors()
     for sensor in sensors:
         data = f"temp+{sensor.get_id()}:{sensor.get_temperature()}"
-        print(data)
         data_queue.put(data)
         loggers["temp"].info(data)
 
 
 @app.on_event("startup")
 async def startup():
-    site.mount_app(app)
     scheduler.start()
 
 
 def getLogger(filename: str):
-    logger = logging.getLogger()
+    logger = logging.getLogger(filename)
     logger.setLevel(logging.INFO)
     formatter = logging.Formatter('%(asctime)s | %(message)s')
-
-    stdout_handler = logging.StreamHandler(sys.stdout)
-    stdout_handler.setLevel(logging.DEBUG)
-    stdout_handler.setFormatter(formatter)
 
     file_handler = logging.FileHandler(filename)
     file_handler.setLevel(logging.DEBUG)
     file_handler.setFormatter(formatter)
 
     logger.addHandler(file_handler)
-    logger.addHandler(stdout_handler)
     return logger
 
 
 if __name__ == '__main__':
     loggers["temp"] = getLogger("logs/temp.log")
 
+    site.mount_app(app)
     uvicorn.run(app, host="0.0.0.0", port=2002)
